99b9b780e35e039e51b61e3e5c44f49e0678635b
Matheus Gonçalves <matheushtht@gmail.com>
2025-06-21 23:10:35 -0300

    Adicionando personagem de testes ao jogo

--- gameplay.c
+++ gameplay.c
@@ -29,12 +29,17 @@ static const int map[MAP_HEIGHT_IN_TILES][MAP_WIDTH_IN_TILES] = {
 //Textura do tileset do mundo
 static Texture2D tileset_texture;
 
+//Textura do player
+static Texture2D player_texture;
+
 void InitGameplay(void) {
     tileset_texture = LoadTexture("assets/graphics/tilesets/tileset.png");
+    player_texture = LoadTexture("assets/graphics/characters/player.png");
 }
 
 void UnloadGameplay(void) {
     UnloadTexture(tileset_texture);
+    UnloadTexture(player_texture);
 }
 
 void UpdateGameplay(void) {
@@ -58,4 +63,9 @@ void RenderGameplay(void) {
             DrawTexturePro(tileset_texture, tileset_source, tileset_dest, (Vector2){0.0f, 0.0f}, 0.0f, WHITE);
         }
     }
+
+    //Desenhando player na tela
+    Rectangle player_source = {0.0f, 0.0f, (float)TILE_SIZE, (float)TILE_SIZE};
+    Rectangle player_dest = {96.0f, 224.0f, (float)TILE_SIZE, (float)TILE_SIZE};
+    DrawTexturePro(player_texture, player_source, player_dest, (Vector2){0.0f, 0.0f}, 0.0f, WHITE);
 }
